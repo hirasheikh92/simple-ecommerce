60bb24a6a477742c94a28f305ad33e00134975f5
hirasheikh92 <hiramoueen@gmail.com>
2023-04-08 16:11:08 +0500

    update files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    CONTENTFUL_SPACE_ID: "6ea3hz5idu8e",
+    CONTENTFUL_ACCESS_KEY: "1kCPEI58pxEAPSr9JC1ilHBI2MJDmzCn3PLo6dtxumc",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
